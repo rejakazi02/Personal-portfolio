723ddc76a27fa6a38985dfc2d598c7e1aa55f2dd
Reja Kazi <rejakazi02@gmail.com>
2022-08-03 23:00:51 +0600

    push

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,44 @@
+<script>
+import AboutMe from '@/components/about/AboutMe';
+import AboutCounter from '@/components/about/AboutCounter';
+import AboutClients from '@/components/about/AboutClients';
+import feather from 'feather-icons';
+
+export default {
+	name: 'About',
+	components: {
+		AboutMe,
+		AboutCounter,
+		AboutClients,
+	},
+	data: () => {
+		return {};
+	},
+	mounted() {
+		feather.replace();
+	},
+	updated() {
+		feather.replace();
+	},
+	methods: {},
+};
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+	<div>
+		<!-- About me -->
+		<div class="container mx-auto">
+			<AboutMe />
+		</div>
+
+		<!-- About counter -->
+		<AboutCounter />
+
+		<!-- About counter -->
+		<div class="container mx-auto">
+			<AboutClients />
+		</div>
+	</div>
 </template>
+
+<style scoped></style>
